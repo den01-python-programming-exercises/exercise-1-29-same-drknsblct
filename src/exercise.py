a37daf501e7f457c623d74196b46c61fb5df3e25
drknsblct <57183092+drknsblct@users.noreply.github.com>
2021-09-17 04:47:53 +0300

    Update exercise.py

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #write your code below this line
+    s1 = input('Enter the first string:')
+    s2 = input('Enter the second string:')
+    
+    if s1 == s2:
+        print('Same')
+    else:
+        print('Different')
 
 if __name__ == '__main__':
     main()
